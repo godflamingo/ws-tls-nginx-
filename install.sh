3f4141808840fcd21486ed1ce607bc001a6861b8
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2022-06-13 13:15:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ fi
 echo -e "\nResoving your domain name...\n"
 dns_ip=`curl -s ipget.net/?ip=$domainName`
 echo -e "Fetching your VPS ip address...\n"
-vps_ip=`curl -s https://ip.gs`
+vps_ip=`curl -s https://ipget.net`
 echo -e "Your domain name is resolved to: $dns_ip\nYour VPS ip address: $vps_ip\n"
 if [ $dns_ip == $vps_ip ]; then
   echo -e "Your domain name has already resolved to the IP address of this VPS! Let's continue.\n"
