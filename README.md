5f3b39bcdfe8d0e74d21a4133a943e5ffff3a5bd
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2022-06-15 16:29:09 +0800

    自动更新SSL证书

--- README.md
+++ README.md
@@ -27,6 +27,8 @@ bash <(curl -L https://raw.githubusercontent.com/windshadow233/ws-tls-nginx/main
 
 该脚本中途使用Let's Encrypt生成证书，脚本会配置自动更新证书的crontab，每日零点检查证书是否将在今日过期，若如是，则更新。
 
+证书更新逻辑：检查/root目录下是否存在.expire-date文件（用以存放证书过期日期的时间戳），若存在则读取其中的时间戳，否则执行certbot命令检查过期日期并写入.expire-date文件。
+
 运行完成后，自动生成客户端配置链接。
 
 部分客户端软件可能会无法识别VLESS链接，此时按链接给出的配置信息手动导入即可。
--- update-ssl.sh
+++ update-ssl.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-expire=`date -d $(certbot certificates -d $1 | grep "Expiry Date" | awk '{print$3}') +%s`
-today=`date +%s`
+if [ ! -f "/root/.expire-date" ]; then
+  expire=`date -d $(certbot certificates -d $1 | grep "Expiry Date" | awk '{print$3}') +%s`
+  echo $expire > /root/.expire-date
+else
+  expire=`cat /root/.expire-date`
+fi
 
+today=`date +%s`
 if [ $today -ge $expire ]; then
-  certbot renew --force-renewal --post-hook "systemctl restart nginx"
+  certbot renew --force-renewal --cert-name $1 --post-hook "systemctl restart nginx"
 fi
