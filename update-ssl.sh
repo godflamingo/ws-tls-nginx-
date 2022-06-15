cdcc2e947f42340a01ee1f7f8984eb9ad1f6a185
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2022-06-15 15:35:23 +0800

    自动更新证书

--- update-ssl.sh
+++ update-ssl.sh
@@ -3,6 +3,6 @@
 expire=`date -d $(certbot certificates -d $1 | grep "Expiry Date" | awk '{print$3}') +%s`
 today=`date +%s`
 
-if [ $today -ge expire ]; then
+if [ $today -ge $expire ]; then
   certbot renew --force-renewal --post-hook "systemctl restart nginx"
 fi
