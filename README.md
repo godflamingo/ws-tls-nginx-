ff0a6444c61fffe90932929ff8c2b194da34354c
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2022-06-17 14:39:12 +0800

    更新脚本

--- README.md
+++ README.md
@@ -12,7 +12,13 @@
 - 域名（解析到VPS的IP）
 
 ### 使用方法
-以root权限执行下面命令并根据脚本提示输入参数。参数分别为：
+以root权限执行下面命令并根据脚本提示输入参数。
+
+```shell
+bash <(curl -L https://raw.githubusercontent.com/windshadow233/ws-tls-nginx/main/install.sh)
+```
+
+参数分别为：
 
 |参数|说明|
 |:-:|:-:|
@@ -21,15 +27,14 @@
 |nginx port|Nginx服务对外开放的端口，也即客户端与服务器通信的端口|
 |domain name|你的域名|
 
-```shell
-bash <(curl -L https://raw.githubusercontent.com/windshadow233/ws-tls-nginx/main/install.sh)
-```
 
 该脚本中途使用Let's Encrypt生成证书，脚本会配置自动更新证书的crontab，每日零点检查证书是否将在n天以后过期，若是，则更新。（n默认为5，如需修改，请在/etc/crontab中修改函数参数，该值默认情况下不应超过30）。
 
 证书更新逻辑：
 1. 检查/root目录下是否存在.<span>$</span>{domainName}-expiredate文件（用以存放证书过期日期的时间戳），若存在则读取其中的时间戳，否则执行certbot获取检查过期日期并写入.<span>$</span>{domainName}-expiredate文件。
 2. 判断当前时间戳是否满足提前天数条件，若为真，则更新证书与时间戳文件。
+
+**注意：80端口将被用于以web-root的方式续约证书，故请始终确保防火墙放行该端口**
 
 运行完成后，自动生成客户端配置链接。
 
--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ `whoami` != "root" ]; then
-  echo "Please run this script with root privileges!"
+  echo -e "\033[31mPlease run this script with root privileges!"
   exit
 fi
-read -p $'Select protocol:\x0a1. VMESS\x0a2. VLESS\x0aChoose 1 or 2: ' protocol
+read -p $'\033[36mSelect protocol:\x0a1. VMESS\x0a2. VLESS\x0aChoose 1 or 2: ' protocol
 case $protocol in
   1)
     protocol=vmess
@@ -13,125 +13,126 @@ case $protocol in
     protocol=vless
     ;;
   *)
-    echo "Wrong input!"
+    echo -e "\033[31mWrong input!"
     exit
 esac
- 
-read -p $'Enter v2ray port (default: 12345)\x0aJust keep the default value if there is no port conflict): ' v2rayPort
+echo -e "\033[36mEnter ports"
+echo -e "\033[31mDO NOT USE 80 ! ! !"
+read -p $'\033[36mEnter v2ray port (default: 12345)\x0aJust keep the default value if there is no port conflict): ' v2rayPort
 export v2rayPort=${v2rayPort:-12345}
-if [[ $v2rayPort -le 0 ]] || [[ $v2rayPort -gt 65535 ]]; then
-  echo "The v2ray port value must be between 1 and 65535."
+if [ $v2rayPort -le 0 ] || [ $v2rayPort -gt 65535 ] || [ $v2rayPort -eq 80 ]; then
+  echo -e "\033[31mThe v2ray port value must be between 1 and 65535. And DO NOT USE 80 ! ! !"
   exit 1
 fi
 
-read -p $'Enter nginx port (default: 443): ' nginxPort
+read -p $'\033[36mEnter nginx port (default: 443): ' nginxPort
 export nginxPort=${nginxPort:-443}
-if [[ $nginxPort -le 0 ]] || [[ $nginxPort -gt 65535 ]]; then
-  echo "The nginx port value must be between 1 and 65535."
+if [ $nginxPort -le 0 ] || [ $nginxPort -gt 65535 ] || [ $nginxPort -eq 80 ]; then
+  echo -e "\033[31mThe nginx port value must be between 1 and 65535. And DO NOT USE 80 ! ! !"
   exit 1
 fi
 
 if [ "$(lsof -i:$v2rayPort | grep LISTEN)" -o "$(lsof -i:$nginxPort | grep LISTEN)" ]; then
-  echo "Port $v2rayPort or $nginxPort is not available."
+  echo -e "\033[31mPort $v2rayPort or $nginxPort is not available."
   exit
 fi
 
-read -p $'Enter your domain name (required): ' domainName
+read -p $'\033[36mEnter your domain name (required): ' domainName
 if [ ! -n "$domainName" ]; then
-  echo "Domain name is required!"
+  echo -e "\033[31mDomain name is required!"
   exit
 fi
 export domainName
 
-echo -e "\nResoving your domain name...\n"
+echo -e "\n\033[36mResoving your domain name...\n"
 dns_ip=`dig A $domainName +short | tail -n 1`
-echo -e "Fetching your VPS ip address...\n"
+echo -e "\033[36mFetching your VPS ip address...\n"
 vps_ip=`curl -s4 https://ipget.net`
-echo -e "Your domain name is resolved to: $dns_ip\nYour VPS ip address: $vps_ip\n"
+echo -e "\033[36mYour domain name is resolved to: $dns_ip\nYour VPS ip address: $vps_ip\n"
 if [ $dns_ip == $vps_ip ]; then
-  echo -e "Your domain name has already resolved to the IP address of this VPS! Let's continue.\n"
+  echo -e "\033[36mYour domain name has already resolved to the IP address of this VPS! Let's continue.\n"
 else
-  echo "Please resolve your domain name to the IP address of this VPS first, and then run this script again."
-  echo "Run 'nslookup $domainName' to check."
+  echo -e "\033[31mPlease resolve your domain name to the IP address of this VPS first, and then run this script again."
+  echo -e "\033[31mRun 'nslookup $domainName' to check."
   exit
 fi
 
-echo -e "Run 'apt-get update'...\n"
+echo -e "\033[36mRun 'apt-get update'...\n"
 apt-get update
 if [ ! "$(command -v v2ray)" ]; then
-  echo -e "Installing V2Ray...\n"
+  echo -e "\033[36mInstalling V2Ray...\n"
   bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 fi
 
 if [ "$(command -v v2ray)" ]; then
-  echo -e "V2Ray was installed.\n"
+  echo -e "\033[36mV2Ray was installed.\n"
 else
-  echo "V2Ray installation failed, please check."
+  echo -e "\033[31mV2Ray installation failed, please check."
   exit
 fi
 
 if [ ! "$(command -v certbot)" ]; then
-  echo -e "Installing Certbot...\n"
+  echo -e "\033[36mInstalling Certbot...\n"
   apt-get -y install certbot
 fi
 
 if [ "$(command -v certbot)" ]; then
-  echo -e "Certbot was installed.\n"
+  echo -e "\033[36mCertbot was installed.\n"
 else
-  echo "Certbot installation failed, please check."
+  echo -e "\033[31mCertbot installation failed, please check."
   exit
 fi
 
 if [ ! "$(command -v nginx)" ]; then
-  echo -e "Installing Nginx...\n"
+  echo -e "\033[36mInstalling Nginx...\n"
   apt-get -y install nginx
 fi
 
 if [ "$(command -v nginx)" ]; then
-  echo -e "Nginx was installed.\n"
+  echo -e "\033[36mNginx was installed.\n"
 else
-  echo "Nginx installation failed, please check."
+  echo -e "\033[31mNginx installation failed, please check."
   exit
 fi
 
-echo -e "Downloading v2ray config...\n"
+echo -e "\033[36mDownloading v2ray config...\n"
 export uuid=`cat /proc/sys/kernel/random/uuid`
 export path=`head -n 10 /dev/urandom | md5sum | head -c $((RANDOM % 10 + 4))`
 curl -sL https://raw.githubusercontent.com/windshadow233/ws-tls-nginx/main/config/v2ray_$protocol.json -o /usr/local/etc/v2ray/config.json.template
 envsubst '${v2rayPort}${uuid}${path}' < /usr/local/etc/v2ray/config.json.template > /usr/local/etc/v2ray/config.json
 rm /usr/local/etc/v2ray/config.json.template
 
-echo -e "Fetching SSL certificate...\n"
+echo -e "\033[36mFetching SSL certificate...\n"
 ufw allow 80
 echo -e 'A' | certbot certonly --register-unsafely-without-email --webroot -w /var/www/html --preferred-challenges http -d $domainName
 
 certificates=`certbot certificates | grep $domainName`
 if [ "$certificates" ]; then
-  echo -e "Certificate was installed successfully!\n"
+  echo -e "\033[36mCertificate was installed successfully! \n"
 else
-  echo "Certificate installation failed, please check."
+  echo -e "\033[31mCertificate installation failed, please check."
   exit
 fi
 
-echo -e "Downloading nginx config...\n"
+echo -e "\033[36mDownloading nginx config...\n"
 curl -sL https://raw.githubusercontent.com/windshadow233/ws-tls-nginx/main/config/nginx.conf -o /etc/nginx/conf.d/v2ray.conf.template
 envsubst '${v2rayPort}${nginxPort}${domainName}${path}' < /etc/nginx/conf.d/v2ray.conf.template > /etc/nginx/conf.d/v2ray.conf
 rm /etc/nginx/conf.d/v2ray.conf.template
 
-echo -e "Downloading certificate automatic renewal script...\n"
+echo -e "\033[36mDownloading certificate automatic renewal script...\n"
 curl -s https://raw.githubusercontent.com/windshadow233/ws-tls-nginx/main/renew-cert.sh -o /root/update-ssl.sh
 chmod +x /root/update-ssl.sh
-echo -e "Writing certificate automatic renewal task into /etc/crontab..."
+echo -e "\033[36mWriting certificate automatic renewal task into /etc/crontab..."
 echo "0 0 * * * root /root/renew-cert.sh $domainName 5" >> /etc/crontab
 
-echo -e "Restarting all services...\n"
+echo -e "\033[36mRestarting all services...\n"
 systemctl daemon-reload
 systemctl restart v2ray
 systemctl restart nginx
 systemctl enable v2ray
 systemctl enable nginx
 ufw allow $nginxPort
-echo -e "Finish! \nV2Ray config file is at: /usr/local/etc/v2ray/config.json\nNginx config file is at: /etc/nginx/conf.d/v2ray.conf\n"
+echo -e "\033[36mFinish! \nV2Ray config file is at: /usr/local/etc/v2ray/config.json\nNginx config file is at: /etc/nginx/conf.d/v2ray.conf\n"
 case $protocol in
   vmess)
     infoStr=`echo "{\"v\": \"2\", \"ps\": \"$domainName\", \"add\": \"$domainName\", \"port\": \"$nginxPort\", \"id\": \"$uuid\", \"aid\": \"0\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"\", \"path\": \"/$path\", \"tls\": \"tls\", \"sni\": \"\"}" | base64 -w 0`
@@ -140,4 +141,4 @@ case $protocol in
     infoStr="$uuid@$domainName:$nginxPort?encryption=none&allowInsecure=false&path=%2F$path&security=tls&type=ws#$domainName"
     ;;
 esac
-echo -e "Import the link shown below to your client software: \n\n$protocol://$infoStr"
+echo -e "\033[32mImport the link shown below to your client software: \n\n$protocol://$infoStr"
